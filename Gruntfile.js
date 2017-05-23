34c99395974cccea98e4190901f1caa7cb5601da
naverstay <naverstay.me@gmail.com>
2017-05-23 16:59:07 +0300

    half

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,85 +1,92 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        watch: {
-            templates: {
-                files: ['jade/*.jade', 'jade/*.pug'],
-                tasks: ['pug'],
-                options: {
-                    spawn: false
-                }
-            },
-            styles: {
-                files: ['sass/*.scss'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false
-                }
-            }
-        },
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'styles/main_global.css': 'sass/main_global.scss'
-                }
-            }
-        },
-        pug: {
-            debug: {
-                options: {
-                    data: {
-                        client: false,
-                        debug: true,
-                        pretty: true
-                    }
-                },
-                files: [{
-                    cwd: "jade/",
-                    src: "*.jade",
-                    dest: "",
-                    expand: true,
-                    ext: ".html"
-                }]
-            },
-            release: {
-                options: {
-                    data: {
-                        client: true,
-                        debug: false,
-                        pretty: false
-                    }
-                },
-                files: [{
-                    cwd: "jade/",
-                    src: "*.jade",
-                    dest: "",
-                    expand: true,
-                    ext: ".html"
-                }]
-            }
-        },
-        jade: {
-            compile: {
-                options: {
-                    client: false,
-                    pretty: true
-                },
-                files: [{
-                    cwd: "jade/",
-                    src: "*.jade",
-                    dest: "",
-                    expand: true,
-                    ext: ".html"
-                }]
-            }
+  grunt.initConfig({
+    watch: {
+      templates: {
+        files: ['jade/*.jade', 'jade/*.pug'],
+        tasks: ['pug'],
+        options: {
+          spawn: false
         }
+      },
+      styles: {
+        files: ['sass/*.scss'],
+        tasks: ['compass'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'styles/main_global.css': 'sass/main_global.scss'
+        }
+      }
+    },
+    pug: {
+      debug: {
+        options: {
+          data: {
+            client: false,
+            debug: true,
+            pretty: true
+          }
+        },
+        files: [{
+          cwd: "jade/",
+          src: "*.jade",
+          dest: "",
+          expand: true,
+          ext: ".html"
+        }]
+      },
+      release: {
+        options: {
+          data: {
+            client: true,
+            debug: false,
+            pretty: false
+          }
+        },
+        files: [{
+          cwd: "jade/",
+          src: "*.jade",
+          dest: "",
+          expand: true,
+          ext: ".html"
+        }]
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          client: false,
+          pretty: true
+        },
+        files: [{
+          cwd: "jade/",
+          src: "*.jade",
+          dest: "",
+          expand: true,
+          ext: ".html"
+        }]
+      }
+    }
 
-    });
+  });
 
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['watch']);
 };
--- js/script.js
+++ js/script.js
@@ -1,4 +1,71 @@
 $(function ($) {
-    console.log(100500);
+  console.log(100500);
+
+  $("header nav > ul > li").hover(
+    function () {
+      var subnav = $(this).children('.sub_nav');
+      var position = $(this).children('a').position();
+      subnav.css('left', position.left - 25);
+
+      subnav.find('.sepor').hide();
+
+      $(this).addClass('select');
+      subnav.stop();
+      subnav.slideDown('fast', function () {
+        subnav.find('.sepor').height(subnav.height());
+        subnav.find('.sepor').fadeIn('fast');
+      });
+
+
+    },
+    function () {
+      var subnav = $(this).children('.sub_nav');
+
+      $(this).removeClass('select');
+      subnav.hide();
+    }
+  );
+
+  $('.copyright .open_popup_box').click(function () {
+    $(this).closest('.copyright').find('.popup_info_box').fadeIn();
+    return false;
+  });
+  $('.copyright .popup_info_box').click(function () {
+    $(this).fadeOut();
+    return false;
+  });
+
+  $("input").each(function (i, el) {
+    var inp = $(this);
+    if (inp.attr('data-inputmask-regex') != void 0) {
+      inp.inputmask('Regex');
+    }
+  });
+
+  $('.valMinus').on('click', function () {
+    var valCell = $(this).closest('.valCell'),
+      inp = valCell.find('input'),
+      val = parseInt(inp.val()),
+      min_val = 1 * inp.attr('data-min'),
+      max_val = 1 * inp.attr('data-max'),
+      new_val = val - (1 * inp.attr('data-step'));
+
+    inp.val(new_val >= min_val ? (new_val <= max_val ? new_val : max_val) : min_val).trigger('change');
+
+    return false;
+  });
+
+  $('.valPlus').on('click', function () {
+    var valCell = $(this).closest('.valCell'),
+      inp = valCell.find('input'),
+      val = parseInt(inp.val()),
+      min_val = 1 * inp.attr('data-min'),
+      max_val = 1 * inp.attr('data-max'),
+      new_val = val + (1 * inp.attr('data-step'));
+
+    inp.val(new_val <= max_val ? (new_val >= min_val ? new_val : min_val) : max_val).trigger('change');
+
+    return false;
+  });
 
 });
